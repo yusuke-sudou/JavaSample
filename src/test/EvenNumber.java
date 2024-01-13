8da1eec646e7a4b7e8d74cdc584e79743bc658a0
yusuke-sudou <sudoukky22@gmail.com>
2024-01-13 14:21:48 +0900

    コメント挿入（テスト1）

--- src/test/EvenNumber.java
+++ src/test/EvenNumber.java
@@ -3,7 +3,7 @@ package test;
 public class EvenNumber {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 1～10までの数字で偶数のみを昇順で表示させる
 		for (int i = 2; i <= 10;  i += 2) {
 			System.out.print(i);
 		}
