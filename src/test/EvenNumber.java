dd3eaef5d3cd8e1409bbc8ce3f3dcb6445c0716f
yusuke-sudou <sudoukky22@gmail.com>
2024-01-13 14:10:47 +0900

    テスト例

---
+++ src/test/EvenNumber.java
@@ -0,0 +1,13 @@
+package test;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 2; i <= 10;  i += 2) {
+			System.out.print(i);
+		}
+
+	}
+
+}
