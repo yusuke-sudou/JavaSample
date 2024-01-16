ee10d754b57a529931e59707613ea4c6ecc65c59
yusuke-sudou <sudoukky22@gmail.com>
2024-01-16 16:45:49 +0900

    テスト問題3

---
+++ src/test/CommaNumbers.java
@@ -0,0 +1,17 @@
+package test;
+
+public class CommaNumbers {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i);
+			if (i < 10) {
+				System.out.print(", ");
+			}
+		}
+
+	}
+
+}
+
+// 1～10までの整数をカンマ区切りで横に表示
