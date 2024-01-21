b84b0d25baebb3ffb64d8613d2792a50667f15f7
yusuke-sudou <sudoukky22@gmail.com>
2024-01-22 08:05:09 +0900

    参考問題１（昇順降順）

--- src/test/Taisaku1.java
+++ src/test/Taisaku1.java
@@ -3,10 +3,14 @@ package test;
 public class Taisaku1 {
 
 	public static void main(String[] args) {
-		for (int i = 2; i <= 10; i+=2) {
-			System.out.println(i);
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
 		}
 
 	}
 
 }
+
+// 1～10を偶数のみ昇順に並べる
---
+++ src/test/Taisaku11.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Taisaku11 {
+
+	public static void main(String[] args) {
+		for (int i = 10; i >= 1; i--) {
+			if (i % 2 == 0) {
+				System.out.println(i);			
+			}
+		}
+
+	}
+
+}
+
+// 1～10まで偶数のみ降順に並べる
