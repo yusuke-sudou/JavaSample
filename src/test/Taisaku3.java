4523624e542ba6a87a5dd3a3d217af370ae3f6f0
yusuke-sudou <sudoukky22@gmail.com>
2024-01-21 18:05:43 +0900

    前日対策

---
+++ src/test/Taisaku3.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Taisaku3 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			if (i < 10) {
+				System.out.print(i + ",");
+			} else {
+				System.out.print(i);
+			}
+		}
+
+	}
+
+}
