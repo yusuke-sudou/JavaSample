657f4250016990ef113c25fb9be5d1da77a3d8f5
yusuke-sudou <sudoukky22@gmail.com>
2024-01-23 12:22:09 +0900

    for文レスト練習

---
+++ src/test/Q001.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Q001 {
+
+	public static void main(String[] args) {
+		for (int i = 9; i >= 1; i--) {
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
