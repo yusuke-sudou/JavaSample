abf7097f40b3b1e556ac0f8679c7f3518dc89061
yusuke-sudou <sudoukky22@gmail.com>
2024-01-24 17:25:02 +0900

    別パターン

--- src/test/Q001.java
+++ src/test/Q001.java
@@ -4,10 +4,25 @@ public class Q001 {
 
 	public static void main(String[] args) {
 		for (int i = 9; i >= 1; i--) {
-			for (int j = i; j >= 1; j--) {
-				System.out.print(j);
+			if (i <= 9 ) {
+				if (i >= 7) {
+					System.out.print(i);
+				}
 			}
 			System.out.println();
+			
+			if (i <= 6) {
+				if (i >= 4) {
+					System.out.print(i);
+				}
+			}
+			System.out.println();
+			
+			if (i <= 3) {
+				if (i >= 1) {
+					System.out.print(i);
+				}
+			}
 		}
 
 	}
